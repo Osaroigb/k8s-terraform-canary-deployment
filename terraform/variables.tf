667aa7620b905034ed95631a237989a892c2a212
Osaro Igbinovia <emmyigbinovia@gmail.com>
2024-10-08 10:40:33 +0100

    feat: automate deployment with GitHub Actions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,48 +18,11 @@ variable "VPC_NAME" {
   default = "whoami-VPC"
 }
 
-# variable "Zone1" {
-#   default = "us-east-1a"
-# }
-
-# variable "Zone2" {
-#   default = "us-east-1b"
-# }
-
-# variable "Zone3" {
-#   default = "us-east-1c"
-# }
-
 variable "VpcCIDR" {
   description = "VPC CIDR block"
   type        = string
   default     = "172.21.0.0/16"
 }
-
-
-# variable "PubSub1CIDR" {
-#   default = "172.21.1.0/24"
-# }
-
-# variable "PubSub2CIDR" {
-#   default = "172.21.2.0/24"
-# }
-
-# variable "PubSub3CIDR" {
-#   default = "172.21.3.0/24"
-# }
-
-# variable "PrivSub1CIDR" {
-#   default = "172.21.4.0/24"
-# }
-
-# variable "PrivSub2CIDR" {
-#   default = "172.21.5.0/24"
-# }
-
-# variable "PrivSub3CIDR" {
-#   default = "172.21.6.0/24"
-# }
 
 variable "private_subnets" {
   description = "Private subnet CIDRs"
